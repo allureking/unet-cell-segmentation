3c27b68c6266b6340265dc96652388403c6d52cc
allureking <kingke0927@gmail.com>
2026-02-20 09:00:00 -0800

    Export ResUNet from model package

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,5 @@
 from src.model.unet import UNet
 from src.model.attention_unet import AttentionUNet
+from src.model.residual_unet import ResUNet
 
-__all__ = ["UNet", "AttentionUNet"]
+__all__ = ["UNet", "AttentionUNet", "ResUNet"]
